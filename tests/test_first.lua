a1ec4d5999a93dbe076f79e555f3c452b91dc896
AgatZan <125749924+AgatZan@users.noreply.github.com>
2024-11-09 23:40:29 +0300

    Update test_first.lua

--- tests/test_first.lua
+++ tests/test_first.lua
@@ -1,16 +1,25 @@
-local T = MiniTest.new_set()
+local function escape(s)
+	return vim.api.nvim_replace_termcodes(s, true, true, true)
+end
+-- H.keys = {
+-- 	above =  escape('<C-o>O'),
+-- 	bs = escape('<bs>'),
+-- 	cr = escape('<cr>'),
+-- 	del = escape('<del>'),
+-- 	keep_undo = escape('<C-g>U'),
+-- 	-- NOTE: use `get_arrow_key()` instead of `H.keys.left` or `H.keys.right`
+-- 	left = escape("<left>"),
+-- 	right = escape("<right>"),
+-- }
 
-T["works"] = function()
-  local x = 1 + 1
-  if x ~= 2 then
-    error("`x` is not equal to 2")
-  end
+local T = MiniTest.new_set({
+      parametrize = { 
+      { escape('<C-o>O') }, { escape('<bs>') }, { escape('<cr>') }, {escape('<C-g>U')}, {escape("<left>")}, {escape("<right>")} 
+    },
+})
+
+T["works"] = function(term)
+    MiniTest.add_note("|1>--- " .. term)
 end
 
-T["not-work"] = function()
-  local x = 2 + 1
-  if x ~= 2 then
-    error("`x` is not equal to 2")
-  end
-end
 return T
